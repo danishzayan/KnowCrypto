357e2e0d92c439c69efb94e9023c4885a2536f29
DanishZayan <danishzayan6@gmail.com>
2023-11-21 01:50:53 -0800

    design searching history list.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,29 +2,39 @@ import React, { useState } from "react";
 import SearchIcon from "../assets/search-icon.svg";
 
 const Search = () => {
-    
-    const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("");
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        let query = e.target.value;
-        setSearchText(query)
-        console.log(query);
-    }
+  const handleChange = (e) => {
+    e.preventDefault();
+    let query = e.target.value;
+    setSearchText(query);
+    console.log(query);
+  };
 
   return (
-    <form className="w-96 relative flex items-center m">
-      <input
-        className="w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-lightgreen"
-        onChange={handleChange}
-        type="text"
-        name="search"
-        placeholder="Search Crypto..."
-      />
-      <button type="submit" className="absolute right-1">
-        <img src={SearchIcon} className="w-[1.4rem]" alt="search" />
-      </button>
-    </form>
+    <>
+      <form className="w-96 relative flex items-center m">
+        <input
+          className="w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-lightgreen"
+          onChange={handleChange}
+          value={searchText}
+          type="text"
+          name="search"
+          placeholder="Search Crypto..."
+        />
+        <button type="submit" className="absolute right-1">
+          <img src={SearchIcon} className="w-[1.4rem]" alt="search" />
+        </button>
+        {
+         searchText.length > 0 ? (
+          <ul className="absolute top-8 right-0 z-50 w-96 h-96 bg-gray-300 border border-slate-600 rounded bg-opacity-60 backdrop-blur-md">
+            <li>Bitcoin</li>
+            <li>Etherium</li>
+          </ul>
+         ) : null
+        }
+      </form>
+    </>
   );
 };
 
