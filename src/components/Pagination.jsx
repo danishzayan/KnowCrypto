7e2c7fdff4173be48af7dbfe5d0ddc8ed2afd166
DanishZayan <danishzayan6@gmail.com>
2024-01-19 10:57:13 -0800

    Add some funtionality.

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,25 +2,25 @@ import React, { useState } from "react";
 import paginationArrow from "../assets/pagination-arrow.svg";
 
 const Pagination = () => {
-  // const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  // const TotalNumber = 250;
+  const TotalNumber = 250;
 
-  // const nextButton = () => {
-  //   if (currentPage === TotalNumber) {
-  //     return null;
-  //   } else {
-  //     setCurrentPage(currentPage + 1);
-  //   }
-  // };
+  const nextButton = () => {
+    if (currentPage === TotalNumber) {
+      return null;
+    } else {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-  // const prevButton = () => {
-  //   if (currentPage === 1) {
-  //     return null;
-  //   } else {
-  //     setCurrentPage(currentPage - 1);
-  //   }
-  // };
+  const prevButton = () => {
+    if (currentPage === 1) {
+      return null;
+    } else {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   // const multiStepNext = () => {
   //   if (currentPage + 3 >= TotalNumber) {
@@ -42,19 +42,19 @@ const Pagination = () => {
     <div className='flex items-center'>
       <ul className="flex items-center justify-end text-sm">
         <li  className='flex items-center'>
-          <button className='outline-0 hover:text-lightgreen w-8'>
+          <button className='outline-0 hover:text-lightgreen w-8' onClick={prevButton}>
             <img className='w-full h-auto rotate-180' src={paginationArrow} alt="leftArrow" />
           </button>
         </li>
-        <li><button>...</button></li>
-        <li><button>1</button></li>
-        <li><button>2</button></li>
-        <li><button>3</button></li>
-        <li><button>...</button></li>
-        <li><button>100</button></li>
+        <li><button className='outline-0 hover:text-lightgreen w-8 h-8 justify-center text-lg'>...</button></li>
+        <li><button onClick={prevButton} className='outline-0 hover:text-lightgreen w-8 h-8 justify-center bg-gray-200 mx-1.5'>{currentPage - 1}</button></li>
+        <li><button className='outline-0  w-8 h-8 justify-center bg-lightgreen text-gray-200 mx-1.5'>{currentPage}</button></li>
+        <li><button onClick={nextButton} className='outline-0 hover:text-lightgreen w-8 h-8 justify-center bg-gray-200 mx-1.5'>{currentPage + 1}</button></li>
+        <li><button className='outline-0 hover:text-lightgreen w-8 h-8 justify-center text-lg'>...</button></li>
+        <li><button className='outline-0 hover:text-lightgreen w-8 h-8 justify-center bg-gray-200 mx-1.5' >{TotalNumber}</button></li>
         <li>
-          <button>
-            <img className='w-full h-auto' src={paginationArrow} alt="leftArrow" />
+          <button className='outline-0 hover:text-lightgreen w-8' onClick={nextButton}>
+            <img className='w-full h-auto' src={paginationArrow} alt="RightArrow" />
           </button>
         </li>
       </ul>
